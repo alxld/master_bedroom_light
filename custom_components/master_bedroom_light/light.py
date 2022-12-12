ca7b490135523473f77fd912df21f77bfdcf71f7
alxld <aaronandlouise@yahoo.com>
2022-12-12 04:59:21 +0000

    Removing 'light' from name

--- custom_components/master_bedroom_light/light.py
+++ custom_components/master_bedroom_light/light.py
@@ -10,6 +10,7 @@ from . import DOMAIN
 sys.path.append("custom_components/new_light")
 from new_light import NewLight
 
+
 async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
@@ -23,12 +24,13 @@ async def async_setup_platform(
     ent = MasterBedroomLight()
     add_entities([ent])
 
+
 class MasterBedroomLight(NewLight):
     """Master Bedroom Light."""
 
     def __init__(self) -> None:
         super(MasterBedroomLight, self).__init__(
-            "Master Bedroom Light", domain=DOMAIN, debug=False, debug_rl=False
+            "Master Bedroom", domain=DOMAIN, debug=False, debug_rl=False
         )
 
         self.entities["light.master_bedroom_group"] = None
